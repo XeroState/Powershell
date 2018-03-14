936d568f883e2cd24e7caa55e0dca3397a53755b
XeroState <37225923+XeroState@users.noreply.github.com>
2018-03-14 11:27:22 -0600

    Add files via upload

---
+++ Upload/CPU_Use.ps1
@@ -0,0 +1,10 @@
+﻿###########################################################################
+# Name: CPU_Use.ps1
+# Get CPU % for each process
+###########################################################################
+
+$Name = Read-Host "What is the PC Name?"
+$Date = Get-Date -format "MM-dd-yyyy_hh-mm-ss"
+$Log = "C:\Temp\$Date.log"
+
+Get-WmiObject -ComputerName $Name Win32_PerfFormattedData_PerfProc_Process | Select-Object Name,PercentProcessorTime | Sort-Object PercentProcessorTime -desc | Out-File $Log
